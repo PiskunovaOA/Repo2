b77750009cf88ed5293cd8396be3b8f3b01ec9c8
Olga <supernatural64@mail.ru>
2022-08-16 14:34:29 +0400

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int randomNumber = GetRandomNumber(100, 999);
+Console.WriteLine(randomNumber);
+
+int secondDigit = (randomNumber / 10) % 10;
+
+Console.Write (secondDigit); 
+
+int GetRandomNumber (int min, int max)
+{
+return new Random().Next(min, max + 1);
+}
